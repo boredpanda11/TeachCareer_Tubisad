458a3f5193d559e25684ad41332a7c6adad434d1
utkukantekin <utkukantekin97@gmail.com>
2022-06-20 21:28:36 +0300

    css manipulation with jquery

--- template.js
+++ template.js
@@ -1,3 +1,12 @@
 let showDate = ()=>{
-    document.getElementById("dateHere").innerHTML = new Date();
+    // document.getElementById("dateHere").innerHTML = new Date();
+    // document.getElementById("dateHere").style.backgroundColor = "red";
+    // document.getElementById("dateHere").style.color = "white";
+    $('#dateHere').html(new Date());
+    const cssObject={
+        "color" : "white",
+        "background-color":"red"
+    }
+    $('#dateHere').css(cssObject);
+
 }
